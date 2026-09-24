d2cf7d1bcf71e42f212e314dc69ec5fbf46f40e2
Sarang RN <rnsarang@gmail.com>
2026-09-24 20:52:14 +0530

    fix: restore Tailwind Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
